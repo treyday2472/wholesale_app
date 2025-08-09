e5f8476e87d206880b8205a09f8991c9e6cd4478
Lawrence Harwell <lharwell@vonage.net>
2025-08-09 13:23:59 -0500

    Added delete button to lead_list

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+csrf = CSRFProtect()  # <-- instantiate
 
 def create_app():
-    from .routes import main
-
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
+    # Optional: print which DB file is being used
     print("DB URI:", app.config['SQLALCHEMY_DATABASE_URI'])
 
-
     db.init_app(app)
+    csrf.init_app(app)  # <-- now works
+
+    from .routes import main
+    app.register_blueprint(main)
 
     with app.app_context():
         db.create_all()
-
-    app.register_blueprint(main)
 
     return app
